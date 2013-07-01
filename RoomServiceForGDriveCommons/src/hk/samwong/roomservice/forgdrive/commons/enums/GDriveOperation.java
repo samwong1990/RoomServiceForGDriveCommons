dd660e26932375d264bb8f971a66dffb98e25bf7
Sam Wong <sam.wong.1990@gmail.com>
2013-07-01 18:27:49 +0100

    API refinements. - FYP submitted version

--- RoomServiceForGDriveCommons/src/hk/samwong/roomservice/forgdrive/commons/enums/GDriveOperation.java
+++ RoomServiceForGDriveCommons/src/hk/samwong/roomservice/forgdrive/commons/enums/GDriveOperation.java
@@ -1,5 +1,5 @@
 package hk.samwong.roomservice.forgdrive.commons.enums;
 
 public enum GDriveOperation {
-	GetGDriveFolders;
+	GetSurroundingFolders, PutFolder, GetMyFolders;
 }
--- RoomServiceForGDriveCommons/src/hk/samwong/roomservice/forgdrive/commons/enums/GDriveParameterKey.java
+++ RoomServiceForGDriveCommons/src/hk/samwong/roomservice/forgdrive/commons/enums/GDriveParameterKey.java
@@ -4,7 +4,7 @@ public enum GDriveParameterKey {
 	// Client to server:
 	OPERATION,		// specifies intent: 
 	OBSERVATION, 	// stores the wifi fingerprint
-	ROOM,			// room name, of course
+	ROOMS,			// list of room names, of course
 	BATCH_TRAINING_DATA,	// self explanatory
 	INSTANCE,		// when validating classification, this is the corresponding instance
 	AUENTICATION_DETAILS,	// self explanatory
@@ -12,5 +12,13 @@ public enum GDriveParameterKey {
 	// Server to client:
 	RETURN_CODE, 	// See Enum ReturnCode
 	ERROR_EXPLANATION, // String with error details
+	
+	
+	
+	// save folder
+	ACCOUNT_NAME,
+	ROOM_NAME,
+	FOLDER_NAME,
+	ALTERNATE_LINK;
 	
 }
